eb50ed2ef6ecb8b54b823891045ac75cbec1266c
Olena Kardash <olena.kardash75@gmail.com>
2024-05-06 12:11:55 +0200

    app localstorage

---
+++ src/Components/Options/Options.jsx
@@ -0,0 +1,11 @@
+export default function Options({updateFeedback, reset, total}) {
+    return (
+        <div>
+            <button onClick={()=> updateFeedback('good')}>Good</button>
+            <button onClick={()=> updateFeedback('neutral')}>Neutral</button>
+            <button onClick={()=> updateFeedback('bad')}>Bad</button>
+            {total>0 && <button onClick={reset}>Reset</button>}
+        </div>
+    );
+    
+}
