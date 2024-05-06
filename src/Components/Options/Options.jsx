b6aabe0c4e476bcb50de9af683f2c89a66fcd807
Olena Kardash <olena.kardash75@gmail.com>
2024-05-06 15:23:25 +0200

    init css

--- src/Components/Options/Options.jsx
+++ src/Components/Options/Options.jsx
@@ -1,6 +1,8 @@
-export default function Options({updateFeedback, reset, total}) {
+import css from "./Options.module.css"
+
+export default function Options({ updateFeedback, reset, total }) {
     return (
-        <div>
+        <div className={css.btn}>
             <button onClick={()=> updateFeedback('good')}>Good</button>
             <button onClick={()=> updateFeedback('neutral')}>Neutral</button>
             <button onClick={()=> updateFeedback('bad')}>Bad</button>
